75918af537716e2ced114fcba9ed353eec78200a
iseunglee <iseunglee99@gmail.com>
2024-03-26 23:50:14 +0900

    psycopg2 복습, 에러 해결 x

--- tutorial.py
+++ tutorial.py
@@ -6,49 +6,51 @@
 # class로 만들기
 
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-class CountyCRUD:
-    def __init__(self):
+load_dotenv()
+DBNAME = os.getenv('dbname')
+USER = os.getenv('user')
+PASSWORD = os.getenv('db_password')
+HOST = os.getenv('host')
+PORT = os.getenv('port')
+
+class CountryCRUD:
+
+    def __init__(self, dbname, user, password, host, port):
+        # 연결 파라미터 설정
         self.conn_params = {
-            'dbname': 'testdb_leesh',
-            'user': 'postgres',
-            'password': 'G=nf_-}EKl',
-            'host': 'hanslab.org',
-            'port': '25432'
+            'dbname' : dbname,
+            'user' : user,
+            'password' : password,
+            'host' : host,
+            'port': port
         }
         self.conn = None
         self.connect()
 
     def connect(self):
-        """데이터베이스에 연결합니다."""
+        '''DB에 연결합니다.'''
         try:
-            self.conn = psycopg2.connect(**self.conn_params)
-            print("데이터베이스에 성공적으로 연결되었습니다.")
+            self.conn = psycopg2.connect(**self.conn_params) # conn_params로 DB 연결 시도
+            print("DB에 성공적으로 연결되었습니다.")
         except psycopg2.Error as e:
-            print(f"데이터베이스 연결 중 오류가 발생했습니다: {e}")
+            print(f"DB 연결 중 오류가 발생했습니다. : {e}")
 
     def create_country(self, country):
-        """country 테이블에 새로운 나라를 추가합니다."""
+        '''country 테이블에 새로운 나라를 추가합니다.'''
         print(country)
-        with self.conn.cursor() as cur:
+        with self.conn.cursor() as cur: # 커서 객체를 사용하여 DB 작업 수행
             cur.execute("""
                 INSERT INTO country (country)
-                VALUES (%s) RETURNING country_id;
-            """, (country,))
-            country_id = cur.fetchone()[0]
-            self.conn.commit()
-            print(f"국가 '{country}'이(가) country {country_id}로 추가되었습니다.")
+                VALUES (%s) RETURNING country_id;""", (country))
+            country_id = cur.fetchone()[0] # 쿼리 결과 country_id 변수에 저장
+            self.conn.commit() # 변경 사항을 DB에 커밋
+            print(f"국가 '{country}'이 (가) country {country_id}로 추가되었습니다.")
             return country_id
-        # with self.conn.cursor() as cur:
-        #     cur.execute("""
-        #         INSERT INTO country (country)
-        #         VALUES (%s);
-        #     """, (country,))
-        #     # country_id = cur.fetchone()[0]
-        #     self.conn.commit()
-        #     # print(f"국가 '{country}'이(가) country {country_id}로 추가되었습니다.")
-        #     # return country_id
-    def read_country(self,country_id):
+        
+    def read_country(self, country_id):
         """country_id 기반으로 국가 정보를 조회합니다."""
         with self.conn.cursor() as cur:
             cur.execute("SELECT * FROM country WHERE country_id = %s;", (country_id,))
@@ -59,15 +61,8 @@ class CountyCRUD:
             else:
                 print("국가를 찾을 수 없습니다.")
                 return None
-    def read_all_country(self):
-        """country_id 기반으로 국가 정보를 전체 조회합니다."""
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT * FROM country;")
-            countrys = cur.fetchall()
-            for country in countrys:
-                print(country)
-
-    def update_country(self, country_id, country):
+    
+    def update_country(self, country_id, country=None):
         """country 정보를 업데이트합니다."""
         with self.conn.cursor() as cur:
             cur.execute("""
@@ -77,19 +72,34 @@ class CountyCRUD:
             """, (country, country_id))
             self.conn.commit()
             print(f"국가 {country_id}의 정보가 업데이트되었습니다.")
-    def delete_country(self,country_id):
-        """영화 정보를 삭제합니다."""
+
+    def delete_country(self, country_id):
+        """country 정보를 삭제합니다."""
         with self.conn.cursor() as cur:
             cur.execute("DELETE FROM country WHERE country_id = %s;", (country_id,))
             self.conn.commit()
             print(f"country {country_id}의 정보가 삭제되었습니다.")
+    
     def close(self):
-        self.conn.close()
+        """데이터베이스 연결을 종료합니다."""
+        if self.conn:
+            self.conn.close()
+            print("데이터베이스 연결이 종료되었습니다.")
 
-country_crud = CountyCRUD()
-country_id = country_crud.create_country("Lion")
+# CountryCRUD 인스턴스 생성
+country_crud = CountryCRUD(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
+
+# 새로운 country 추가
+country_id = country_crud.create_country("mountain")
+
+# country 정보 조회
 country_crud.read_country(country_id)
-country_crud.read_all_country()
-country_crud.update_country(country="Tiger", country_id=country_id )
+
+# country 정보 업데이트
+country_crud.update_country(country_id, country="mountain")
+
+# country 정보 삭제
 country_crud.delete_country(country_id)
+
+# 데이터베이스 연결 종료
 country_crud.close()
